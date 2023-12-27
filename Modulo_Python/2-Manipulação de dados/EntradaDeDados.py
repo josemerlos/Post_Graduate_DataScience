15d3f70da6388345524b784c9572acf395d28ace
José Carlos Merlos Junior <50033164+josemerlos@users.noreply.github.com>
2023-12-27 19:41:47 -0300

    Update EntradaDeDados.py

--- Modulo_Python/2-Manipulação de dados/EntradaDeDados.py
+++ Modulo_Python/2-Manipulação de dados/EntradaDeDados.py
@@ -1,3 +1,19 @@
 nome = input("Digite seu nome: ")
 nascimento = input("Digite seu ano de nascimento: ")
 email = input("Digite seu emai: ")
+
+print("Nome: ")
+print(nome)
+
+print("Nascimento: ")
+print(nascimento)
+
+print("E-mail: ")
+print(email)
+
+
+
+#OU:
+
+print("Nome: " , nome , ". E-mail: ", email , ". Nascimento: ", nascimento)
+
